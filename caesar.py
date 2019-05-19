afb11362fbda355df03d0981aa80113dc680a466
theresaoh <49175564+theresaoh@users.noreply.github.com>
2019-05-19 16:29:44 -0600

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,5 +1,17 @@
 from helpers import alphabet_position, rotate_character
 
 def encrypt(text, rot):
-    #Add your code here
-    return
+#encrypts full strings of text by returning each character "rot" places
+#to the right of each character in the text in alphabetical order 
+    result = ""
+    for char in text:
+        result += rotate_character(char, rot)
+    return result
+    
+def main():
+    text = input("Type a message: ")
+    rot = int(input("Rotate by: "))
+    print(encrypt(text, rot))
+
+if __name__ == "__main__":
+    main()
