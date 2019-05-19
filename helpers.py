fe7a11b93054d0dae976a7bd269f65c58b5a2a7d
theresaoh <49175564+theresaoh@users.noreply.github.com>
2019-05-19 16:30:43 -0600

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,10 +1,18 @@
-
+alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 def alphabet_position(letter):
-    #Add your code here
-    return
-
+#returns the numerical index of a letter in the alphabet, 0-25
+    for i in range(len(alphabet)):
+        if alphabet[i] == letter.lower():
+            return i
 
 def rotate_character(char, rot):
-    #Add your code here
-    return
+#returns the letter "rot" places to the right of "char" in the alphabet
+#if char is not an alphabetical character, just char is returned
+    if not char.isalpha():
+        return char
+    char_index = alphabet_position(char)
+    rot_index = (char_index + rot) % 26
+    if char.isupper() == True:
+        return alphabet[rot_index].upper()
+    return alphabet[rot_index]
